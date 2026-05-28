38e5aa62d994c80aee294c092806039ea41c113b
[dyad] <hoteityoussef20@gmail.com>
2026-05-28 19:01:37 +0300

    Reverted all changes back to version c827375870b09cc5012d46fa0c769930554e941a

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,12 +17,9 @@ const Hero = () => {
         }}
       >
         <div className="absolute inset-0 bg-coffee-petrol/60 backdrop-blur-[2px]" />
-        
-        {/* White Fade Bottom */}
-        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-white to-transparent z-10" />
       </div>
 
-      <div className="container mx-auto px-6 relative z-20">
+      <div className="container mx-auto px-6 relative z-10">
         <div className="max-w-3xl">
           <motion.div
             initial={{ opacity: 0, y: 30 }}
@@ -59,7 +56,7 @@ const Hero = () => {
       <motion.div 
         animate={{ y: [0, -15, 0] }}
         transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
-        className="absolute bottom-10 right-10 hidden lg:block z-20"
+        className="absolute bottom-10 right-10 hidden lg:block"
       >
         <div className="relative">
           <div className="absolute -inset-4 bg-coffee-yellow/20 rounded-full blur-xl animate-pulse" />
@@ -68,7 +65,7 @@ const Hero = () => {
       </motion.div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center text-coffee-cream/50 z-20">
+      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center text-coffee-cream/50">
         <span className="text-[10px] uppercase tracking-widest mb-2">Scroll</span>
         <div className="w-[1px] h-12 bg-gradient-to-b from-coffee-yellow to-transparent" />
       </div>
