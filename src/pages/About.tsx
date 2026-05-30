15824b59544b02d7ca7a5d7461ed48b2c20fc7dc
[dyad] <hoteityoussef20@gmail.com>
2026-05-30 11:46:30 +0300

    [dyad] Fixing missing Link import in About page - wrote 1 file(s)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { Bean, Users, Sparkles, Coffee, ArrowRight, ArrowDown } from 'lucide-react';
